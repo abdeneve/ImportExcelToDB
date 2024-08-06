bbecc95498eddd3c42025a65b44d9d3915c5ad5c
alex.salazar <alex.salazar@iatec.com>
2024-08-05 23:27:19 -0300

    REFACTOR: The BulkInsertAsync method has been refactored

---
+++ src/Infrastructure/BulkInsert.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Infrastructure;
+public class BulkInsert : IBulkInsert
+{
+    public async Task BulkInsertAsync(string connectionString, DataTable dataTable)
+    {
+        using SqlConnection connection = new SqlConnection(connectionString);
+        await connection.OpenAsync();
+        using var bulkCopy = new SqlBulkCopy(connection);
+
+        bulkCopy.DestinationTableName = "Sale";
+        bulkCopy.BatchSize = 10000;
+
+        bulkCopy.ColumnMappings.Add("Id", "SaleId");
+        bulkCopy.ColumnMappings.Add("CountryId", "CountryId");
+        bulkCopy.ColumnMappings.Add("CustomerId", "CustomerId");
+        bulkCopy.ColumnMappings.Add("StoreCustomerCode", "StoreCustomerCode");
+        bulkCopy.ColumnMappings.Add("CustomerItemCode", "CustomerItemCode");
+        bulkCopy.ColumnMappings.Add("Date", "Date");
+        bulkCopy.ColumnMappings.Add("Quantity", "Quantity");
+        bulkCopy.ColumnMappings.Add("Amount", "Amount");
+
+        try
+        {
+            await bulkCopy.WriteToServerAsync(dataTable);
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
